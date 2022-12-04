dbe7884c85aa91f1f9ea888573e5bce9248e1496
johnsmiiiiiith <111529540+johnsmiiiiiith@users.noreply.github.com>
2022-12-04 19:22:57 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,18 +1,31 @@
 <?php
-    $destino= "jpetro@uade.edu.ar";
-    $nombre= $_POST["nombre"];
-    $apellido= $_POST["apellido"];
-    $telefono= $_POST["telefono"];
-    $mail= $_POST["mail"];
-    $mensaje= $_POST["mensaje"];
+    
+    $nombre= $_POST[¨nombre¨];
+    $apellido= $_POST[¨apellido¨];
+    $telefono= $_POST[¨telefono¨];
+    $mail= $_POST[¨mail¨];
+    $mensaje= $_POST[¨mensaje¨];
+    
+    //como me llega el mensaje
+    
+    //como me llegar el mensaje
+
+    $mensaje = ¨Este mensaje fue enviado:¨. $nombre . ¨,\r\n¨;
+    $mensaje = ¨Su apellido es:¨. $apellido. ¨,\r\n¨;
+    $mensaje = ¨Su telefono es:¨. $telefono. ¨,\r\n¨;
+    $mensaje = ¨Su mail es:¨. $mail. ¨,\r\n¨;
+    $mensaje = ¨Su mensaje es:¨. $mensaje. ¨,\r\n¨;
+
+    $para = ¨jpetro@uade.edu.ar¨;
+    $asunto = ¨Este mensaje fue enviadeo desde tu cv.¨;
+    
+    //funcion mail
+    mail($para, $asunto, utf8_decode($mensaje), $header);
+    //redireccion
+    header(¨gracias.html¨);
     
 
-    $header = "Enviado desde la página de contacto:CV";
-    $mensajeCompleto = $nombre . "\nnombre:" . $apellido."\napellido:" . $telefono . "\ntelefono:" . $mail. "\nmail:" . $mensaje. "\nmensaje:";
-
-    mail($destinatario, $asunto, $mensajeCompleto, $header)
-    echo "<script>alert("Enviado exitosamente.")</script>";
-    echo "<script> setTimeout(\"location.href="gracias.html"\",1000)</script>"
+    
 
 
 ?>
